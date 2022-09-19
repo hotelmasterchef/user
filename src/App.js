40b37437bcd7ac8aadac7047b2d268b53e186c4f
sahoochinmay <sahoochinmay54@gmail.com>
2022-09-19 12:28:39 +0530

    About page work continue

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Menu from "./pages/Menu";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/menu" element={<Menu />} />
+        <Route exact path="/about" element={<About />} />
       </Routes>
       <Footer />
     </BrowserRouter>
