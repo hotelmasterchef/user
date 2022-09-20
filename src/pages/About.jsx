b53384a4544f6a548c77d1b996522e15daf13d0a
sahoochinmay <sahoochinmay54@gmail.com>
2022-09-20 09:16:01 +0530

    Basic about us page completed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -20,8 +20,22 @@ const About = () => {
               containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
             </p>
           </Col>
-          <Col lg="6" sm="12" style={{border: "1px solid white"}}>
-            <img src={Img_restaurant} alt="Restaurant" className="about_restaurant"  />
+          <Col lg="6" sm="12" className="d-flex justify-content-center justify-center align-center ">
+            <img src={Img_restaurant} alt="Restaurant" className="about_restaurant_img" />
+          </Col>
+        </Row>
+        <Row style={{ marginTop: "100px" }}>
+          <Col lg="6" sm="12" className="d-flex justify-content-center justify-center align-center ">
+            <img src={Img_restaurant} alt="Restaurant" className="about_restaurant_img" />
+          </Col>
+          <Col lg="6" sm="12">
+            <p className="mt-5">
+              <h1>Hi...</h1>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the
+              1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but
+              also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
           </Col>
         </Row>
       </Container>
